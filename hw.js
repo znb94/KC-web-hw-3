d3712a597a786f49f7a02a4a3992b527fc5a9a16
zainab <zainabdashty2@gmail.com>
2022-08-26 14:42:54 +0300

    done

---
+++ hw.js
@@ -0,0 +1,42 @@
+let stuff = ["rice cake ", "coco shia granola" , "straberry granola"]
+
+stuff.forEach(x => { 
+    alert(x);
+stuff.innerhtml
+
+});
+
+
+
+
+const stuf =  [
+{stuf :"wordpress" , price : 0.900},
+{stuf :"wordpress" , price : 2.00},
+{stuf :"wordpress" , price : 2.00},
+
+
+]
+
+let total = 0 
+
+stuf.forEach(iteam => {
+
+
+total += iteam.price;
+
+
+  }  );
+
+console.log("total :total");
+
+const totalprice = stuf.reduce((total , item) => {
+return total = item.price ;
+
+
+
+
+},0   );
+
+
+console.log("total: totalprice");
+
